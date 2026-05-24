db66fc9c40a7b92012d60ce029e5157b6b230f96
shahriyarmonshe-alt <shahriyarmonshe@gmail.com>
2026-05-24 19:16:09 +1000

    Update server.js

--- server.js
+++ server.js
@@ -38,12 +38,7 @@ const supabase = createClient(
 
 // Middleware
 app.use(cors({
-  origin: [
-    'https://itech3208-project-1-multilingual-di.vercel.app',
-    'http://localhost:3000',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500'
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
