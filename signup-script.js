bcca76c5853c29b4795ce33db4ec702ff53afec8
shahriyarmonshe-alt <shahriyarmonshe@gmail.com>
2026-05-25 00:25:23 +1000

    Update signup-script.js

--- signup-script.js
+++ signup-script.js
@@ -1,6 +1,6 @@
 // Signup Script for StoryBond
 
-const API_URL = 'https://itech3208-project-1-multilingual-digital-reflection-fmlrod095.vercel.app';
+const API_URL = 'https://itech3208-project-1-multilingual-digital-reflection-72kqb5g7x.vercel.app';
 
 const CONFIG = {
   MIN_PASSWORD: 6,
