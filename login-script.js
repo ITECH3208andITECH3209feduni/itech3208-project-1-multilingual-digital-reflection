a8a5d075cca1ae84320977290638f6b8c5986685
shahriyarmonshe-alt <shahriyarmonshe@gmail.com>
2026-05-26 01:53:47 +1000

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -7,7 +7,7 @@ const CONFIG = {
   DELAY_MS:      1400,
 };
 
-const API_URL = 'https://itech3208-project-1-multilingual-digital-reflection-72kqb5g7x.vercel.app';
+const API_URL = 'https://itech3208-project-1-multilingual-digital-reflection-29vwrayye.vercel.app';
 
 const DOM = {
   
